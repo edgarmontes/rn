cebce8a37626b66dc00a63ca8babd43313198e6e
React <react_programmer@Edgars-Air.lan1>
2020-01-30 23:14:44 -0600

    React Native Components Part 1

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Main from "./components/MainComponent";
+
+export default function App() {
+  return <Main />;
+}
+/*
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HELLO WORLD !!!</Text>
     </View>
   );
 }
@@ -12,8 +19,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#bdc3c7" //,
+    //alignItems: "center",
+    //justifyContent: "center"
+  }
 });
+*/
